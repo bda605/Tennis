45a5d1dddbfb27230925db83d31a102474d6ed07
LIONMAIL\eddiechen <bda605@gmail.com>
2018-12-12 17:48:34 +0800

    加入專案檔案。

---
+++ TennisTest/Tennis.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisTest
+{
+    public class Tennis
+    {
+        private readonly string _firstPlayerName;
+        private readonly string _secondPlayerName;
+
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {0,"Love" },
+            {1,"Fifteen" },
+            {2,"Thirty" },
+            {3,"Forty" }
+        };
+
+        public Tennis(string firstPlayerName, string secondPlayerName)
+        {
+            _firstPlayerName = firstPlayerName;
+            _secondPlayerName = secondPlayerName;
+        }
+
+        public string Score(int firstPlayerScore, int secondPlayerScore)
+        {
+            if (IsScoreDiff(firstPlayerScore, secondPlayerScore))
+            {
+                if (firstPlayerScore > 3 || secondPlayerScore > 3)
+                {
+                    if (Math.Abs(firstPlayerScore - secondPlayerScore) == 1)
+                    {
+                        var advPlayer = AdvPlayer(firstPlayerScore, secondPlayerScore);
+                        return $"{advPlayer} Adv";
+                    }
+
+                    return $"{AdvPlayer(firstPlayerScore, secondPlayerScore)} Win";
+                }
+
+                return $"{_scoreLookup[firstPlayerScore]} {_scoreLookup[secondPlayerScore]}";
+            }
+            if (IsDeuce(firstPlayerScore))
+            {
+                return Deuce();
+            }
+            return SameScore(firstPlayerScore);
+        }
+
+        private string AdvPlayer(int _firstPlayerScoreTimes, int _secondPlayerScoreTimes)
+        {
+            return _firstPlayerScoreTimes > _secondPlayerScoreTimes
+                ? _firstPlayerName
+                : _secondPlayerName;
+        }
+
+        private static string Deuce()
+        {
+            return "Deuce";
+        }
+
+        private static bool IsDeuce(int _firstPlayerScoreTimes)
+        {
+            return _firstPlayerScoreTimes >= 3;
+        }
+
+        private string SameScore(int _firstPlayerScoreTimes)
+        {
+            return $"{_scoreLookup[_firstPlayerScoreTimes]} All";
+        }
+
+        private static bool IsScoreDiff(int _firstPlayerScoreTimes, int _secondPlayerScoreTimes)
+        {
+            return _firstPlayerScoreTimes != _secondPlayerScoreTimes;
+        }
+    }
+}
---
+++ TennisTest/TennisTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace TennisTest
+{
+    public class TennisTest
+    {
+        private Tennis _tennis = new Tennis("Bob", "Jack");
+
+        [Test]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All", 0, 0);
+        }
+
+        [Test]
+        public void Fifteen_Love()
+        {
+            ScoreShouldBe("Fifteen Love", 1, 0);
+        }
+
+        [Test]
+        public void Thirty_Love()
+        {
+            ScoreShouldBe("Thirty Love", 2, 0);
+        }
+        [Test]
+        public void Forty_Love()
+        {
+            ScoreShouldBe("Forty Love", 3, 0);
+        }
+
+        [Test]
+        public void Love_Fifteen()
+        {
+            ScoreShouldBe("Love Fifteen", 0, 1);
+        }
+
+        [Test]
+        public void Love_Thirty()
+        {
+            ScoreShouldBe("Love Thirty", 0, 2);
+        }
+
+        [Test]
+        public void Love_Forty()
+        {
+            ScoreShouldBe("Love Forty", 0, 3);
+        }
+
+        [Test]
+        public void Fifteen_All()
+        {
+            ScoreShouldBe("Fifteen All", 1, 1);
+        }
+
+        [Test]
+        public void Thirty_All()
+        {
+            ScoreShouldBe("Thirty All", 2, 2);
+        }
+
+        [Test]
+        public void Deuce()
+        {
+            ScoreShouldBe("Deuce", 3, 3);
+        }
+
+        [Test]
+        public void FirstPlayer_Adv()
+        {
+            ScoreShouldBe("Bob Adv", 4, 3);
+        }
+
+        [Test]
+        public void SecondPlayer_Adv()
+        {
+            ScoreShouldBe("Jack Adv", 3, 4);
+        }
+
+        [Test]
+        public void FirstPlayer_Win()
+        {
+            ScoreShouldBe("Bob Win", 5, 3);
+        }
+
+        [Test]
+        public void SecondPlayer_Win()
+        {
+            ScoreShouldBe("Jack Win", 3, 5);
+        }
+
+
+        private void ScoreShouldBe(string expected, int firstPlayerScore, int secondPlayerScore)
+        {
+            NUnit.Framework.Assert.AreEqual(expected, _tennis.Score(firstPlayerScore, secondPlayerScore));
+        }
+    }
+}
